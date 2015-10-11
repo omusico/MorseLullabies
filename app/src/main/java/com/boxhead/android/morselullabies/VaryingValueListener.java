39f43e0e26cd023279e8923c9ca1c2905dbfa065
Paul Rowe <boxhead852@gmail.com>
2015-10-11 12:28:40 +0800

    convert to gradle

---
+++ app/src/main/java/com/boxhead/android/morselullabies/VaryingValueListener.java
@@ -0,0 +1,7 @@
+package com.boxhead.android.morselullabies;
+
+interface VaryingValueListener {
+
+	void onValueChanged(double value2);
+
+}
--- src/com/boxhead/android/morselullabies/VaryingValueListener.java
+++
@@ -1,7 +0,0 @@
-package com.boxhead.android.morselullabies;
-
-interface VaryingValueListener {
-
-	void onValueChanged(double value2);
-
-}
